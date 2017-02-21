02abe5520d96e693403d0d699dd7c2c60c68f5fc
mojtaba moayedi <moayedim93@gmail.com>
2017-02-21 11:21:19 +0330

    insert key farsi

--- index.php
+++ index.php
@@ -76,6 +76,20 @@ $keyboard_keys = [
     'X',
     'Y',
     'Z',
+    //insert farsi
+    'َش',
+    'س',
+    'ی',
+    'ب',
+    'ل',
+    'ا',
+    'ت',
+    'ن',
+    'م',
+    'ک',
+    'گ',
+    'ظ',
+    'ط',
 ];
 include_once ('config.php');
 $categories = $conn->query('SELECT * FROM categories');
